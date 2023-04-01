6fa3643bbc93b3f43fb19c9a6154ba6a73957987
Tim Ruscica <mrtechguy234@gmail.com>
2023-04-01 16:16:31 -0300

    add unit test for index route

---
+++ test.py
@@ -0,0 +1,6 @@
+from main import app
+
+def test_index_route():
+    response = app.test_client().get("/")
+    assert response.status_code == 200
+    assert response.data.decode('utf-8') == "Hello, World!"!
