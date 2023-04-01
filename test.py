85fc280d398fc101eea25b1bb4e7c2099ade68d8
Tim Ruscica <mrtechguy234@gmail.com>
2023-04-01 16:28:09 -0300

    fix test action/workflow

--- test.py
+++ test.py
@@ -3,4 +3,4 @@ from main import app
 def test_index_route():
     response = app.test_client().get("/")
     assert response.status_code == 200
-    assert response.data.decode('utf-8') == "Hello, World!"!
+    assert response.data.decode('utf-8') == "Hello, World!"
